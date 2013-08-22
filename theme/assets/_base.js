d9dcc69d10de2939b4e4b23179407c3cbac80324
Darryn Soper <darryn@luciddesign.co.nz>
2013-08-22 17:01:24 +1200

    Add product image switcher script to _base.js

--- theme/assets/_base.js
+++ theme/assets/_base.js
@@ -57,3 +57,15 @@ $(function() {
     html: true
   });
 });
+
+/* Product Image Switcher */
+$('.thumbnail').click(function() {
+	var targetImage = $(this).attr('data-main-image');
+		$('#main').fadeOut(400, function(){
+			$('div.loader').fadeIn(100);
+			$(this).attr('src', targetImage).load(function(){
+				$('div.loader').fadeOut(100);
+				$(this).fadeIn();
+			});
+		});
+});
