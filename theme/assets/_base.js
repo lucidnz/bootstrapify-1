9cba7ff44fe30014cb0a6cea4d94f4d987c57916
Darryn Soper <darryn@luciddesign.co.nz>
2013-08-22 17:08:04 +1200

    move image switcher script out of base.js and into app.js

--- theme/assets/_base.js
+++ theme/assets/_base.js
@@ -58,14 +58,4 @@ $(function() {
   });
 });
 
-/* Product Image Switcher */
-$('.thumbnail').click(function() {
-	var targetImage = $(this).attr('data-main-image');
-		$('#main').fadeOut(400, function(){
-			$('div.loader').fadeIn(100);
-			$(this).attr('src', targetImage).load(function(){
-				$('div.loader').fadeOut(100);
-				$(this).fadeIn();
-			});
-		});
-});
+
