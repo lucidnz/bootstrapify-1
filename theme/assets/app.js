a62baeecd4be0c11bc736f0f5f9feac263c48fc0
Stewart Knapman <info@stewartknapman.com>
2013-08-29 16:58:39 +1200

    move login and register forms into partials

--- theme/assets/app.js
+++ theme/assets/app.js
@@ -9,3 +9,35 @@ $('.thumbnail').click(function() {
 		});
 	});
 });
+
+
+/* Recover password form */
+var recoverForm = document.getElementById('customer-recover-password-form'),
+  loginForm = document.getElementById('customer-login-form');
+
+function showRecoverPasswordForm() {
+  $(recoverForm).closest('.customer-recover-password-form').show();
+  $(loginForm).closest('.customer-login-form').hide();
+}
+
+function hideRecoverPasswordForm() {
+  $(recoverForm).closest('.customer-recover-password-form').hide();
+  $(loginForm).closest('.customer-login-form').show();
+}
+
+$('.hide-recover-password-form').on('click', function(e){
+  hideRecoverPasswordForm();
+  e.preventDefault();
+});
+
+$('.show-recover-password-form').on('click', function(e){
+  showRecoverPasswordForm();
+  e.preventDefault();
+});
+
+// dont show links if we dont have both includes present
+if(recoverForm === null){ document.getElementById('forgotten-password-link').style.display='none'; }
+if(loginForm === null){ document.getElementById('recover-password-link').style.display='none'; }
+
+hideRecoverPasswordForm();
+if (window.location.hash === '#recover') { showRecoverPasswordForm(); }
