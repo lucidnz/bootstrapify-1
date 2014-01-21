d5c5e309c533967be3ff2656cbe2a628e85d1b7b
Stewart Knapman <info@stewartknapman.com>
2014-01-21 16:59:36 +1300

    Re hash the option selectors js

---
+++ theme/assets/bootstrapify-option-selection.js
@@ -0,0 +1,113 @@
+// ---------------------------------------------------------------------------
+// Bootstrapify option selector by Lucid - 17/1/2014
+// ---------------------------------------------------------------------------
+
+Shopify.BootstrapifyOptionSelectors = function(existingSelectorId, options){
+  // override markup vars
+  this.b_selectorDivClass       = 'selector-wrapper form-group';
+  this.b_selectorClass          = 'single-option-selector form-control';
+  // call base constructor
+  Shopify.BootstrapifyOptionSelectors.baseConstructor.call(this, existingSelectorId, options);
+  // apply markup
+  this.bootstrapifyMarkup();
+  // linked options
+  if(this.product.available && this.product.options.length > 1){
+    Shopify.linkOptionSelectors(this.product);
+  }
+};
+
+Shopify.extend(Shopify.BootstrapifyOptionSelectors, Shopify.OptionSelectors);
+
+Shopify.BootstrapifyOptionSelectors.prototype.bootstrapifyMarkup = function(){
+  var self = this;
+  Shopify.each(this.selectors, function(selector){
+    var element = selector.element;
+    var parentElement = element.parentNode;
+    element.className = self.b_selectorClass;
+    parentElement.className = self.b_selectorDivClass;
+  });
+};
+
+
+
+// (c) Copyright 2011 Caroline Schnapp. All Rights Reserved. Contact: mllegeorgesand@gmail.com
+// See http://wiki.shopify.com/Linked_Options
+ 
+var Shopify = Shopify || {};
+ 
+Shopify.optionsMap = {};
+ 
+Shopify.updateOptionsInSelector = function(selectorIndex) {
+    
+  switch (selectorIndex) {
+    case 0:
+      var key = 'root';
+      var selector = jQuery('.single-option-selector:eq(0)');
+      break;
+    case 1:
+      var key = jQuery('.single-option-selector:eq(0)').val();
+      var selector = jQuery('.single-option-selector:eq(1)');
+      break;
+    case 2:
+      var key = jQuery('.single-option-selector:eq(0)').val();  
+      key += ' / ' + jQuery('.single-option-selector:eq(1)').val();
+      var selector = jQuery('.single-option-selector:eq(2)');
+  }
+  
+  var initialValue = selector.val();
+  selector.empty();    
+  var availableOptions = Shopify.optionsMap[key];
+  for (var i=0; i<availableOptions.length; i++) {
+    var option = availableOptions[i];
+    var newOption = jQuery('<option></option>').val(option).html(option);
+    selector.append(newOption);
+  }
+  if (jQuery.inArray(initialValue, availableOptions) !== -1) {
+    selector.val(initialValue);
+  }
+  selector.trigger('change');  
+  
+};
+ 
+Shopify.linkOptionSelectors = function(product) {
+  // Building our mapping object.
+  for (var i=0; i<product.variants.length; i++) {
+    var variant = product.variants[i];
+    if (variant.available) {
+      // Gathering values for the 1st drop-down.
+      Shopify.optionsMap['root'] = Shopify.optionsMap['root'] || [];
+      Shopify.optionsMap['root'].push(variant.option1);
+      Shopify.optionsMap['root'] = Shopify.uniq(Shopify.optionsMap['root']);
+      // Gathering values for the 2nd drop-down.
+      if (product.options.length > 1) {
+        var key = variant.option1;
+        Shopify.optionsMap[key] = Shopify.optionsMap[key] || [];
+        Shopify.optionsMap[key].push(variant.option2);
+        Shopify.optionsMap[key] = Shopify.uniq(Shopify.optionsMap[key]);
+      }
+      // Gathering values for the 3rd drop-down.
+      if (product.options.length === 3) {
+        var key = variant.option1 + ' / ' + variant.option2;
+        Shopify.optionsMap[key] = Shopify.optionsMap[key] || [];
+        Shopify.optionsMap[key].push(variant.option3);
+        Shopify.optionsMap[key] = Shopify.uniq(Shopify.optionsMap[key]);
+      }
+    }
+  }
+  // Update options right away.
+  Shopify.updateOptionsInSelector(0);
+  if (product.options.length > 1) Shopify.updateOptionsInSelector(1);
+  if (product.options.length === 3) Shopify.updateOptionsInSelector(2);
+  // When there is an update in the first dropdown.
+  jQuery(".single-option-selector:eq(0)").change(function() {
+    Shopify.updateOptionsInSelector(1);
+    if (product.options.length === 3) Shopify.updateOptionsInSelector(2);
+    return true;
+  });
+  // When there is an update in the second dropdown.
+  jQuery(".single-option-selector:eq(1)").change(function() {
+    if (product.options.length === 3) Shopify.updateOptionsInSelector(2);
+    return true;
+  });
+  
+};
